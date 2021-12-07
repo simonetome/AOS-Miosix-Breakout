c9c833d218037f159c5277eeb1f7be83a9b27bff
gioeleverze <gioele.verze@mail.polimi.it>
2021-12-07 16:24:22 +0100

    test ball and paddle

--- main.cpp
+++ main.cpp
@@ -5,62 +5,148 @@
 #include <termios.h>
 #include <unistd.h>
 #include "miosix.h"
+#include <thread>
 
 using namespace std;
 using namespace miosix;
 
+//thread for input
+void thread_function()
+{
+    char c,lastC;
+    struct termios t;
+    int x=15;
+    	printf("\x1b[%d;%dH###",15,x-1);
+	fflush(stdout);
+	printf("\x1b[999;999H");     //cursor to the last cell of the terminal
+    	fflush(stdout);
+    for(;;)
+    {
+	//set option to take in input only one character
+    	tcgetattr(STDIN_FILENO,&t);
+        t.c_lflag &= ~ECHO;
+        t.c_lflag &= ~(ICANON);
+        tcsetattr(STDIN_FILENO,TCSANOW,&t);
+        
+        c=getchar();
+
+        tcgetattr(STDIN_FILENO,&t);
+        t.c_lflag |= ECHO;
+        t.c_lflag |= (ICANON);
+        tcsetattr(STDIN_FILENO,TCSANOW,&t);
+        //move right
+        if(c=='d')
+    	{
+    		if(lastC=='d')
+    		{
+    			x++;
+    			printf("\x1b[%d;%dH ###",15,x-1);}
+    		else{
+    			printf("\x1b[%d;%dH ###",15,x-2);
+    			x--;
+    		}
+		fflush(stdout);
+		printf("\x1b[999;999H");
+	    	fflush(stdout);
+	    	lastC=c;
+    		
+    	}else if(c=='a') //move left
+    	{
+    		if(lastC=='a')
+    		{
+    			x--;
+    			printf("\x1b[%d;%dH### ",15,x-2);}
+		else{	
+
+    			printf("\x1b[%d;%dH### ",15,x-1);
+    			x++;
+    		}
+		fflush(stdout);
+		printf("\x1b[999;999H");
+	    	fflush(stdout);
+	    	lastC=c;
+    		
+    	}
+
+    	c=' ';
+    }
+}
+
 
 int main()
 {	
-	int lastX=1;
+	int lastX=3;
 	char grid[15][31];
 	bool avanti=true;
 
+	printf("\e[1;1H\e[2J\n");//clear screen
 	for(int i=0;i<15;i++){
 		for(int j=0;j<30;j++)
 		{
 			
 			if(j==0 || j==29)
-				grid[i][j]='#';
+				grid[i][j]='|';
 				else
-			grid[i][j]=' ';
-			if(i==14)
+			if(i<3)
 				grid[i][j]='#';
+			else
+				grid[i][j]=' ';
+			if(i==14)
+				grid[i][j]='_';
 		}
 
 		grid[i][30]='\n';
 
 	}
-	for(;;){
-	printf("\e[1;1H\e[2J\n");
+	grid[14][0]='.';
+	grid[14][29]='.';
 	
-	if(lastX==29)
+	
+	//wait to start the game
+		struct termios t;
+		printf("press any key to start the game");
+		tcgetattr(STDIN_FILENO,&t);
+		t.c_lflag &= ~(ICANON);
+		tcsetattr(STDIN_FILENO,TCSANOW,&t);
+		
+		getchar();
+
+		tcgetattr(STDIN_FILENO,&t);
+		t.c_lflag |= (ICANON);
+		tcsetattr(STDIN_FILENO,TCSANOW,&t);      
+        
+        printf("\e[1;1H\e[2J\n");
+	printf("%s\n",grid);
+	std::thread th(&thread_function);
+	
+	for(;;){
+	
+	if(lastX==20)
 	{
 		avanti=false;
 	}
-	if(lastX==1)
+	if(lastX==3)
 		avanti=true;
 	if(avanti)
 	{	
-		grid[0][lastX-1]=' ';
-		grid[0][lastX]='.';
-		grid[0][0]='#';
+		printf("\x1b[%d;%dH",5,lastX);//print alle coordinate (5,lastX)
+		printf(" ○");
+    		fflush(stdout);
 		lastX++;
 	}else
 	{
-		grid[0][lastX+1]=' ';
-		grid[0][lastX]='.';
-		grid[0][29]='#';
-		grid[0][30]='\n';
+		printf("\x1b[%d;%dH",5,lastX);
+		printf("○ ");
+    		fflush(stdout);
 		lastX--;
 	}
-	
-		printf("%s\n",grid);
-		
-		//fflush(stdout);
-		Thread::sleep(50);
+		printf("\x1b[999;999H");
+    		fflush(stdout);
+
+		Thread::sleep(100);
 		
 	}
+
 }
 
 
